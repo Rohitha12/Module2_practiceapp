776a20605e35f6f1227dfab02caa85675eac8f0a
Rohitha <rohitha.meda216@gmail.com>
2020-03-02 10:28:12 -0600

    Commit Changes

--- practice.js
+++ practice.js
@@ -2,5 +2,5 @@ function app()
 {
 num1 = document.getElementById("number 1").value;
 num2 = document.getElementById("number 2").value;
-document.getElementById("result").innerHTML = num1 * num2;
+document.getElementById("result").innerHTML = num1 % num2;
 }
